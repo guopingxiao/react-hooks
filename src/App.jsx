2e59a030a31ae38476bf2f4c985c30150e0b24c7
xiaoguoping <xiaoguoping@meituan.com>
2020-02-13 22:47:37 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,55 @@
-import React, { useState, useMemo, useCallback, memo } from 'react';
+import React, { useState, useMemo, useRef, useCallback, useEffect, PureComponent } from 'react';
 import './App.css';
 
-const Counter = memo(function Counter(props) {
-  console.log('Counter click')
-  return (
-    <h1 onClick={props.onClick}>useContext:{props.count}</h1>
-  )
-})
+// const Counter = memo(function Counter(props) {
+//   console.log('Counter click')
+//   return (
+//     <h1 onClick={props.onClick}>useContext:{props.count}</h1>
+//   )
+// })
+
+class Counter extends PureComponent { 
+
+  speak() { 
+    console.log(`now count is ${this.props.count}`)
+  }
+  render() { 
+    const { props } = this
+    return (
+      <h1 onClick={props.onClick}>useContext:{props.count}</h1>
+    )
+  }
+}
 
 function App() {
-  
   const [count, setCount] = useState(0)
   const [clickCount, setClickCount] = useState(0)
   // 只有在count===3前后两次变化的时候依赖项才会变化，double才会重新计算
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const double = useMemo(() => count * 2, [count === 3])
   
-  const half = useMemo(() => { return double / 4 }, [double])
-  
-  // const onClick = useMemo(() => { 
-  //   return () => { 
-  //     console.log('click')
-  //   }
-  // },[]) 
+  const counterRef = useRef()
+  let it = useRef()
 
-    const onClick = useCallback(() => { 
-      console.log('click')
-      // 一般事件都会改变状态的
-      setClickCount((clickCount)=> clickCount+1)
-  },[]) 
+  const onClick = useCallback(() => { 
+    console.log('click')
+    // 一般事件都会改变状态的
+    setClickCount((clickCount) => clickCount + 1)
+    // console.log(counterRef.current)
+    counterRef.current.speak()
+  }, [counterRef]) 
+  
+  useEffect(() => {
+    it.current = setInterval(() => { 
+      setCount(count=> count + 1)
+    },1000)
+  }, [])
+
+  useEffect(() => { 
+    if (count >= 10) {
+      clearInterval(it.current)
+    }
+  })
   
   return (
       <div>
@@ -37,8 +58,8 @@ function App() {
           onClick={() => setCount(count + 1)}>
           Add
         </button>
-      <Counter onClick={onClick} count={double}></Counter>
-      double:{double} === half: {half},  clickCount:{clickCount}
+      <Counter ref={counterRef} onClick={onClick} count={double}></Counter>
+      count:{count}, double:{double},  clickCount:{clickCount}
       </div>
     )
 
