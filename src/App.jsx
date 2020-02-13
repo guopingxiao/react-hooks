4bd7d829f474b5d46c4a3cc0ca23faa127f573bb
xiaoguoping <xiaoguoping@meituan.com>
2020-02-13 21:50:21 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,34 @@
-import React, { useState, createContext, Component, useContext } from 'react';
+import React, { useState, useMemo, useCallback, memo } from 'react';
 import './App.css';
 
-const CountContext = createContext()
-
-class Foo extends Component { 
-  render() { 
-    return (
-      <CountContext.Consumer>
-        {
-          count => <h1>Consumer:{count}</h1>
-        }
-      </CountContext.Consumer>
-    )
-  }
-}
-
-class Bar extends Component { 
-  static contextType = CountContext
-  render() { 
-    const count = this.context
-    return (
-          <h1>contextType:{count}</h1>
-    )
-  }
-}
-
-function Counter() { 
-  const count = useContext(CountContext)
+const Counter = memo(function Counter(props) {
+  console.log('Counter click')
   return (
-    <h1>useContext:{count}</h1>
+    <h1 onClick={props.onClick}>useContext:{props.count}</h1>
   )
-}
+})
 
 function App() {
   
   const [count, setCount] = useState(0)
+  const [clickCount, setClickCount] = useState(0)
+  // 只有在count===3前后两次变化的时候依赖项才会变化，double才会重新计算
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const double = useMemo(() => count * 2, [count === 3])
+  
+  const half = useMemo(() => { return double / 4 }, [double])
+  
+  // const onClick = useMemo(() => { 
+  //   return () => { 
+  //     console.log('click')
+  //   }
+  // },[]) 
+
+    const onClick = useCallback(() => { 
+      console.log('click')
+      // 一般事件都会改变状态的
+      setClickCount((clickCount)=> clickCount+1)
+  },[]) 
   
   return (
       <div>
@@ -43,11 +37,8 @@ function App() {
           onClick={() => setCount(count + 1)}>
           Add
         </button>
-      <CountContext.Provider value={count}>
-        <Foo></Foo>
-        <Bar></Bar>
-        <Counter></Counter>
-        </CountContext.Provider>
+      <Counter onClick={onClick} count={double}></Counter>
+      double:{double} === half: {half},  clickCount:{clickCount}
       </div>
     )
 
